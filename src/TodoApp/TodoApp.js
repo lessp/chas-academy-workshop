f34c546fdec3d93f831e247f1aac4d62ef77832b
Tom Ekander <tom.ekander@iteam.se>
2019-03-08 12:09:10 +0100

    feat: finished project for workshop

--- src/TodoApp/TodoApp.js
+++ src/TodoApp/TodoApp.js
@@ -4,9 +4,97 @@ import { checkIcon, deleteIcon, todoEmpty } from './img/icons'
 import Todo from '../Todo.module.css'
 
 function TodoApp() {
+  const [inputValue, setInputValue] = React.useState('')
+  const [todos, setTodos] = React.useState([])
+  const [activeFilter, setActiveFilter] = React.useState('All')
+
+  const availableFilters = ['All', 'Completed', 'Not completed']
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    if (inputValue !== '') {
+      setTodos([
+        ...todos,
+        {
+          text: inputValue,
+          done: false,
+        },
+      ])
+
+      setInputValue('')
+    }
+  }
+
+  const toggleTodo = todoToToggle => {
+    setTodos(
+      todos.map(t =>
+        t.text === todoToToggle.text
+          ? {
+              ...t,
+              done: !todoToToggle.done,
+            }
+          : t
+      )
+    )
+  }
+
+  const byActiveFilter = todo => {
+    switch (activeFilter) {
+      case 'All':
+        return true
+      case 'Completed':
+        return todo.done
+      case 'Not completed':
+        return !todo.done
+    }
+  }
+
   return (
     <div className={Todo.wrapper}>
-      <h1 className={Todo.title}>Testing, testing...</h1>
+      <h1 className={Todo.title}>What do I need to do!</h1>
+
+      {/* Input */}
+      <form onSubmit={handleSubmit}>
+        <input
+          className={Todo.input}
+          type="text"
+          value={inputValue}
+          placeholder="Add something to do..."
+          onChange={e => setInputValue(e.target.value)}
+        />
+        <button className={Todo.primaryButton} type="submit">
+          Add
+        </button>
+      </form>
+
+      {/* Filters */}
+      <ul className={Todo.filters}>
+        {availableFilters.map(f => (
+          <li
+            key={f}
+            className={f === activeFilter ? Todo.filterActive : Todo.filter}
+            onClick={_event => setActiveFilter(f)}>
+            {f}
+          </li>
+        ))}
+      </ul>
+
+      {/* List todos */}
+      <ul className={Todo.list}>
+        {todos.filter(byActiveFilter).map(t => (
+          <li key={t.text} className={Todo.item}>
+            <p className={Todo.todoItem}>{t.text}</p>
+            <button className={Todo.icon} onClick={_event => toggleTodo(t)}>
+              {t.done ? (
+                <img alt="Incomplete" src={deleteIcon} />
+              ) : (
+                <img alt="Complete" src={checkIcon} />
+              )}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- src/TodoApp/__tests__/TodoApp.spec.js
+++ src/TodoApp/__tests__/TodoApp.spec.js
@@ -1,10 +1,103 @@
 import React from 'react'
-import { render, fireEvent, wait } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 
 import TodoApp from '../TodoApp'
 
 describe('TodoApp', () => {
   test('it renders', () => {
-    expect(true).toBe(false)
+    const { getByText } = render(<TodoApp />)
+
+    const title = getByText(/what do i need to do!/i)
+
+    expect(title).toBeInTheDocument()
+  })
+
+  test('we can add a todo', () => {
+    const { getByText, getByPlaceholderText } = render(<TodoApp />)
+    const inputField = getByPlaceholderText('Add something to do...')
+    fireEvent.click(inputField)
+    fireEvent.change(inputField, {
+      target: {
+        value: 'Write more tests!',
+      },
+    })
+    fireEvent.click(getByText(/add/i))
+
+    expect(getByText(/write more tests!/i)).toBeInTheDocument()
+  })
+
+  test('we can mark a todo as complete', () => {
+    const { getByText, getByPlaceholderText, getByAltText } = render(
+      <TodoApp />
+    )
+    const inputField = getByPlaceholderText('Add something to do...')
+
+    fireEvent.click(inputField)
+    fireEvent.change(inputField, {
+      target: {
+        value: 'Write more tests!',
+      },
+    })
+    fireEvent.click(getByText(/add/i))
+
+    fireEvent.click(getByAltText('Complete'))
+
+    expect(getByAltText('Incomplete')).toBeInTheDocument()
+  })
+
+  test('we can mark a todo as incomplete', () => {
+    const { getByText, getByPlaceholderText, getByAltText } = render(
+      <TodoApp />
+    )
+    const inputField = getByPlaceholderText('Add something to do...')
+
+    fireEvent.click(inputField)
+    fireEvent.change(inputField, {
+      target: {
+        value: 'Write more tests!',
+      },
+    })
+    fireEvent.click(getByText(/add/i))
+
+    fireEvent.click(getByAltText('Complete'))
+
+    expect(getByAltText('Incomplete')).toBeInTheDocument()
+
+    fireEvent.click(getByAltText('Incomplete'))
+
+    expect(getByAltText('Complete')).toBeInTheDocument()
+  })
+
+  test('we can filter todos', () => {
+    const {
+      getByText,
+      getByPlaceholderText,
+      getByAltText,
+      queryByText,
+    } = render(<TodoApp />)
+    const inputField = getByPlaceholderText('Add something to do...')
+    fireEvent.click(inputField)
+    fireEvent.change(inputField, {
+      target: {
+        value: 'Write more tests!',
+      },
+    })
+    fireEvent.click(getByText(/add/i))
+
+    fireEvent.click(getByAltText('Complete'))
+
+    expect(getByAltText('Incomplete')).toBeInTheDocument()
+
+    /* Click on the completed filter */
+    fireEvent.click(getByText('Completed'))
+
+    /* The todo should be here */
+    expect(getByText('Write more tests!')).toBeInTheDocument()
+
+    /* Click on the 'not completed' filter */
+    fireEvent.click(getByText('Not completed'))
+
+    /* The todo should not be here since it's completed */
+    expect(queryByText('Write more tests!')).not.toBeInTheDocument()
   })
 })
